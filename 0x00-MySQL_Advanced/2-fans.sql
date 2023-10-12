940102aaf4bcff2c764be9b73f030e9468e1645f
timmoh-king <timothykariuki43@gmail.com>
2023-10-12 09:42:40 +0300

    2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,14 +1,7 @@
--- Create a temporary table to store the rankings
-CREATE TEMPORARY TABLE band_rankings AS
-SELECT origin, SUM(nb_fans) AS total_fans
+-- Write a SQL script that ranks country origins of bands
+-- ordered by the number of (non-unique) fans
+
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
-GROUP BY origin;
-
--- Rank the origins based on the total number of fans
-SET @rank = 0;
-SELECT @rank := @rank + 1 AS rank, origin, total_fans
-FROM band_rankings
-ORDER BY total_fans DESC;
-
--- Clean up the temporary table
-DROP TEMPORARY TABLE band_rankings;
+GROUP BY origin
+ORDER BY nb_fans DESC;
