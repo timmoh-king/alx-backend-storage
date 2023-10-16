910d851a2afb67ef10a772e58096c15b0d3eee6a
timmoh-king <timothykariuki43@gmail.com>
2023-10-16 07:45:18 +0300

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,6 +2,6 @@
 -- ordered by the number of (non-unique) fans
 
 SELECT origin, SUM(fans) AS nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+	FROM metal_bands
+	GROUP BY origin
+	ORDER BY nb_fans DESC;
---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+-- Write a SQL script that lists all bands with Glam rock
+-- as their main style, ranked by their longevity
+
+-- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
