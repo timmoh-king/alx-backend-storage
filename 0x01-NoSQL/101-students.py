551854dd62cb91c1045733dacc25dfb38ddbfd47
timmoh-king <timothykariuki43@gmail.com>
2023-10-20 10:06:29 +0300

    101-students.py

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -10,4 +10,12 @@
 
 def top_students(mongo_collection):
     """returns all students sorted by average score:"""
-
+    db.mongo_collection.aggregate([
+        {
+            '$project': {
+                'name': 1,
+                'averageScore': {'$avg': '$topics.score'}
+                }
+            },
+        {'$sort': {'avarageScore': -1}}
+        ])
