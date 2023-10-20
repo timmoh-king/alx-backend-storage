3b8c179bbf334d77e0762c5fd07c1e3dfb1c0a20
timmoh-king <timothykariuki43@gmail.com>
2023-10-20 09:54:17 +0300

    12-log_stats.py

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+    a Python function that returns all students sorted by average score:
+    Prototype: def top_students(mongo_collection):
+    mongo_collection will be the pymongo collection object
+    The top must be ordered
+    The average score must be part of each item returns with key = avgScore
+"""
+
+def top_students(mongo_collection):
+    """returns all students sorted by average score:"""
+
